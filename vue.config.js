6c778c42245e23ebc90dd688dcc3789cd2e928b8
Erick Madrigal Flores <e.madrigal@cheilmex.local>
2023-04-05 14:06:58 -0600

    se quita propiedad public

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     disableHostCheck: true,
-    public: 'coeli-be-run-z34ajjggwa-uc.a.run.app'
+    // public: 'coeli-be-run-z34ajjggwa-uc.a.run.app'
   }
 })
