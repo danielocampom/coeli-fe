dbc83d860a1040fef46dabb8a6e744a0dba46ee7
Daniel Ocampo <carlosdanielocampo62@gmail.com>
2023-04-05 15:00:41 -0600

    Cambios en Host

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    // disableHostCheck: true,
+    disableHostCheck: true,
     host: 'coeli-fe-z34ajjggwa-uc.a.run.app'
   }
 })
