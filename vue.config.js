1cb1af5a61fe906885e2022c59009c08b6c98996
Erick Madrigal Flores <e.madrigal@cheilmex.local>
2023-04-05 13:38:51 -0600

    solucion header

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true,
+    public: 'coeli-be-run-z34ajjggwa-uc.a.run.app'
+  }
 })
